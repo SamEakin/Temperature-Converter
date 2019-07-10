fda4ad8b0a19a33e04705935ddfd6a16cb715a0f
hcghanta <hghanta@unomaha.edu>
2019-07-10 15:52:51 -0500

    new validations

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -16,53 +16,72 @@ namespace TemperatureConverter
     {
         static string getInput()
         {
-            Console.WriteLine("Enter a Temperature in the format 11f or 11c");
+            Console.Write("Enter a Temperature in the format 11f or 11c: ");
             string input = Console.ReadLine();
             return input;
         }
 
-        static int ConvertInput(string input)
+        static string ConvertInput(string input)
         {
-            int temp;
-            string rawInput = input.Replace("c", "");
-            rawInput = input.Replace("f", "");
-            bool check = Int32.TryParse(rawInput, out temp); //fails if input is say "ddf" so trying to use TryParse
-            if (check)
-                Console.WriteLine("See the result below:");
-            else
-            {
-                Console.WriteLine("Conversion cannot be done");
-                //should start from the beginning asking for user input 
-            }
-            return temp;
+            string rawInput = input.ToUpper();
+            rawInput = rawInput.Replace("C", "");
+            rawInput = rawInput.Replace("F", "");
+            return rawInput;
         }
 
         static void Convert()
         {
+            int temp;
             string input = getInput();
             Console.WriteLine(input);
-            int temp = ConvertInput(input);         
-            string result = "0";
-            if (input.EndsWith("c"))
+            string tempString = ConvertInput(input);
+            bool check = Int32.TryParse(tempString, out temp); //fails if input is say "ddf" so trying to use TryParse
+            if (check)
             {
-                Console.WriteLine("Celcius");               
-                result = (9 * (temp + 32) / 5) + "F";               
-            }
-            else if (input.EndsWith("f"))
-            {
-                Console.WriteLine("Fahrenheit to Celsius conversion");             
-                result = (5 * (temp - 32) / 9) + "C";
+                Console.WriteLine("See the result below:");
+                string result = "0";
+                if (input.EndsWith("c"))
+                {
+                    Console.WriteLine("Celcius to Fahrenheit conversion");
+                    result = (9 * (temp + 32) / 5) + "F";
+                }
+                else if (input.EndsWith("f"))
+                {
+                    Console.WriteLine("Fahrenheit to Celsius conversion");
+                    result = (5 * (temp - 32) / 9) + "C";
+                }
+
+                Console.WriteLine("{0} equals to {1}", input, result);
+                // Console.WriteLine($"Result:{result}");
+                ReRun();
+                
             }
             else
             {
-                Console.WriteLine("Please Enter a valid input format");
+                Console.WriteLine("Conversion cannot be done. Please specify proper format");
                 Convert();
             }
-
-            Console.WriteLine($"Result:{result}");
         }
 
-
+        static void ReRun()
+        {
+            Console.Write("Do you want to continue?(Y/N): ");
+            string response = Console.ReadLine();
+            if (response == "Y" || response == "y")
+            {
+                Convert();
+                Console.ReadLine();
+            }
+            else if (response == "N" || response == "n")
+            {
+                Console.WriteLine("Thank you for using the application. See you soon!");
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid response");
+                ReRun();
+            }
+        }
 
         static void Main(string[] args)
         {
