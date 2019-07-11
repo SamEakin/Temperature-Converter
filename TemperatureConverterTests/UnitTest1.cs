3bba209d71cb5ba4595630476661a9f92a6dd76e
seakin <sseakin@gmail.com>
2019-07-11 15:50:42 -0500

    Created unit tests for Temperature.Convert()

---
+++ TemperatureConverterTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using TemperatureConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemperatureConverterTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void FahrenheitConvertTest()
+        {
+            Fahrenheit temp = new Fahrenheit(0);
+            int result = temp.Convert();
+            Assert.AreEqual(-17, result);
+        }
+
+        [TestMethod]
+        public void CelsiusConvertTest()
+        {
+            Celsius temp = new Celsius(0);
+            int result = temp.Convert();
+            Assert.AreEqual(32, result);
+        }
+
+        /*
+        [TestMethod]
+        public void RemoveInputUnitIdentifierTest()
+        {
+            int result = RemoveInputUnitIdentifier("10f");
+            Assert.AreEqual(10, result);
+        }
+        */
+    }
+}
